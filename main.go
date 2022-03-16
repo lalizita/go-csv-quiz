5ab2facac8e7700051b30b6ec1b8447d60b4ef53
lalizita <laislima98@hotmail.com>
2022-03-16 19:47:10 -0300

    Fix read input value

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"encoding/csv"
 	"fmt"
 	"io"
@@ -10,66 +9,41 @@ import (
 )
 
 type QuizQuestion struct {
-	Order    int
 	Question string
 	Result   string
-}
-
-type Answers struct {
-	Order      int
-	UserAnswer string
-	isCorrect  bool
-}
-
-func readCSVQuiz() ([][]string, error) {
-	csvFile, err := os.Open("test.csv")
-	if err != nil {
-		log.Fatal("ERRO AO LER CSV:", err)
-	}
-
-	defer csvFile.Close()
-
-	r := csv.NewReader(csvFile)
-	return r.ReadAll()
+	Answer   string
 }
 
 func main() {
-	records, err := readCSVQuiz()
+	f, err := os.Open("test.csv")
+	if err != nil {
+		log.Fatal("Opening file:", err)
+	}
 
-	allQuestions := make([]QuizQuestion, 0)
-	answers := make([]Answers, 0)
-	totalCorrect := 0
-
+	c := csv.NewReader(f)
+	r, err := c.ReadAll()
 	if err != nil {
 		log.Fatal("Erro ao retornar CSV:", err)
 	}
 	if err == io.EOF {
 		log.Fatal("CSV vazio")
 	}
-	for index, r := range records {
-		allQuestions = append(allQuestions, QuizQuestion{Order: index, Question: r[0], Result: r[1]})
-		answers = append(answers, Answers{Order: index})
+
+	allQuestions := make([]QuizQuestion, 0)
+	for _, r := range r {
+		allQuestions = append(allQuestions, QuizQuestion{Question: r[0], Result: r[1]})
 	}
 
-	// totalQuestions := len(allQuestions)
-	scanner := bufio.NewScanner(os.Stdin)
-	for _, q := range allQuestions {
-		fmt.Printf("Questão %d - %s\n", q.Order+1, q.Question)
-		scanner.Scan()
-		answers[q.Order].UserAnswer = scanner.Text()
-	}
-
-	for i, a := range answers {
-		if a.UserAnswer == allQuestions[i].Result {
-			a.isCorrect = true
+	totalCorrect := 0
+	for index, q := range allQuestions {
+		fmt.Printf("Questão %d - %s\n", index+1, q.Question)
+		fmt.Scan(&allQuestions[index].Answer)
+		if allQuestions[index].Answer == allQuestions[index].Result {
 			totalCorrect++
-		} else {
-			a.isCorrect = false
 		}
 	}
 
 	fmt.Println("TOTAL DE QUESTÕES: ", len(allQuestions))
 	fmt.Println("===================")
 	fmt.Println("CERTAS: ", totalCorrect)
-	fmt.Println("ERRADAS: ", len(allQuestions)-totalCorrect)
 }
